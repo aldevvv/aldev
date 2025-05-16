c0731ec098836688c9df40b0efefb99c72ad40b5
Muhammad Alif <mhdalif.id@gmail.com>
2025-05-16 23:01:36 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var typed = new Typed('#typed-text', {
+    strings: ['Open Source Developer', 'Graphic Designer', 'Philosophy &amp; Poetry Enthusiast'],
+    typeSpeed: 60,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+  });
+});
+
